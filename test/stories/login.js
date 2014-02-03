eaeee4cc1ce11ff7941e1a9e9054251ccb08eafb
Droogans <droogans@gmail.com>
2014-02-03 16:19:45 -0600

    Use mocha in spec files.

--- test/stories/login.js
+++ test/stories/login.js
@@ -1,41 +1,42 @@
 var basePage = require('../pages/Base.js'),
-    loginPage = require('../pages/login/Form.js');
+    loginPage = require('../pages/login/Form.js'),
+    expect = require('./setupExpect').expect;
 
 describe('Login page', function(){
 
-    it('beforeAll', function() {
+    before(function() {
         basePage.go();
         basePage.btnLogin.click();
     });
 
     it('should not log you in if username and password are blank', function() {
         loginPage.login('', '');
-        expect(basePage.isLoggedIn()).toBe(false);
+        expect(basePage.isLoggedIn()).to.eventually.be.false;
     });
 
     it('should not log you in if username is blank', function() {
         loginPage.login('', 'password');
-        expect(basePage.isLoggedIn()).toBe(false);
+        expect(basePage.isLoggedIn()).to.eventually.be.false;
     });
 
     it('should not log you in if password is blank', function() {
         loginPage.login('username', '');
-        expect(basePage.isLoggedIn()).toBe(false);
+        expect(basePage.isLoggedIn()).to.eventually.be.false;
     });
 
     it('should display an error if both username and password are incorrect', function() {
         loginPage.login('INVALID', 'INVALID');        
-        expect(loginPage.lblLoginError.isDisplayed()).toBe(true);
+        expect(loginPage.lblLoginError.isDisplayed()).to.eventually.be.true;
     });
 
     it('should allow a user to login', function() {
         loginPage.login();
-        expect(basePage.isLoggedIn()).toBe(true);
+        expect(basePage.isLoggedIn()).to.eventually.be.true;
     });
 
     it('should allow a user to sign out', function() {
         basePage.btnLogout.click();
-        expect(basePage.isLoggedIn()).toBe(false);
+        expect(basePage.isLoggedIn()).to.eventually.be.false;
     });
 
 });
