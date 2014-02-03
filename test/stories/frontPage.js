4470ce2b161e429faba802a05cf68ac0aa0eefb5
Droogans <droogans@gmail.com>
2014-02-03 15:23:46 -0600

    Use mocha in spec files.

--- test/stories/frontPage.js
+++ test/stories/frontPage.js
@@ -1,24 +1,25 @@
 var basePage = require('../pages/Base'),
-    loginPage = require('../pages/login/Form.js')
+    loginPage = require('../pages/login/Form.js'),
+    expect = require('./setupExpect').expect;
 
 describe('Main page', function() {
 
     basePage.go();
 
     it('should be at the correct URL', function() {
-        expect(basePage.currentUrl).toEqual(browser.baseUrl + basePage.url);
+        expect(basePage.currentUrl).to.eventually.equal(browser.baseUrl + basePage.url);
     });
 
     it('should let me click through to the login page', function() {
         basePage.btnLogin.click();
         // We switch to the loginPage here because we've navigated to it
-        expect(loginPage.currentUrl).toEqual(browser.baseUrl + loginPage.url);
+        expect(loginPage.currentUrl).to.eventually.equal(browser.baseUrl + loginPage.url);
     });
 
     it('should return to the front page when clicking the Epik Vote link', function() {
         // You can't call the loginPage here, because lnkEpikVote belongs to the basePage
         basePage.lnkEpikVote.click();
-        expect(basePage.currentUrl).toEqual(browser.baseUrl + basePage.url);
+        expect(basePage.currentUrl).to.eventually.equal(browser.baseUrl + basePage.url);
     });
 
 });
